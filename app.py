0f1063921c7e683c1ecacf4f967b4eb98ce62039
Vijay Rodrigues <vijayrodrigues18@gmail.com>
2021-12-21 20:34:56 +0530

    first commit

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
@@ -39,4 +38,3 @@ def contact():
 
 if __name__=='__main__':
     app.run(debug=True)
-    
